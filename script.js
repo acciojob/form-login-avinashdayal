eddfef28e7ac1541dd4995e7488c8ed42fdf9d80
Avinash Dayal <avinashdayal1608@gmail.com>
2025-12-01 22:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    event.preventDefault(); // stops page refresh
 
+    // get form inputs
+    let firstName = document.forms["form1"]["fname"].value;
+    let lastName = document.forms["form1"]["lname"].value;
+
+    // show full name
+    alert( firstName + " " + lastName);
 }
